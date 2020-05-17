10dab2bad32b1cf7aab9084bc0f5429892cf1840
yairks <y.k.sachs@gmail.com>
2020-05-17 13:36:04 -0400

    ==

--- js/javascript_tests.js
+++ js/javascript_tests.js
@@ -2,7 +2,7 @@ window.onload = function execute() {
     console.log("Hello, World!");
     let div_elems = document.getElementsByClassName("img");
     for(let i = 0; i < div_elems.length; i++) {
-        let img = div_elems[i].getElementsByTagName("img")[0];
+        let   img = div_elems[i].getElementsByTagName("img")[0];
         //let margin_left = parseInt(div_elems[i].getAttribute("margin-left"));
         //let margin_right = div_elems[i].getAttribute("margin");
         console.log(div_elems[i].width);
